41dcb3d7b503f5628967e3206f0681200a8df5d6
vk17205@gmail.com <heok17@gmail.com>
2023-04-13 16:01:45 +0530

    change3

--- vk.java
+++ vk.java
@@ -2,6 +2,6 @@ public class wel
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Seo ye-chae & Jo jeong-hyeok");
+		System.out.println("Seo ye-chae & Jo jeong-hyeok!!");
 	}
 }
